eeb3b94a1033bce2672f7e4320a1af5a92a5ac46
Emil Məmmədov <emill.memmedovv@gmail.com>
2022-03-04 09:10:55 +0400

    create book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\UserBook;
 use App\Traits\ApiResponder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class BookController extends Controller
 {
@@ -25,5 +29,30 @@ class BookController extends Controller
             ]);
 
         return $this->respond('Book List', $books);
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function create(Request $request): JsonResponse
+    {
+        $this->validate($request,[
+            'title',
+            'description'
+        ]);
+
+        DB::transaction(function ($query) use ($request) {
+            $book = Book::query()->create([
+                'title' => $request->get('title'),
+                'description' => $request->get('description')
+            ]);
+
+            UserBook::query()->create([
+                'book_id' => $book->getKey(),
+                'user_id' => Auth::id()
+            ]);
+        });
+
+        return $this->respond('Book saved');
     }
 }
